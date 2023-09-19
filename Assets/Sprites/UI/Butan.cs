dcb9c927fab1548ff77926e4cad5ea5db28d18d1
Perz <s.ali224@mybvc.ca>
2023-09-19 09:09:23 -0600

    Big UI WOOOOOO
    Big UI WOOOOOO

---
+++ Assets/Sprites/UI/Butan.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Butan : MonoBehaviour,IPointerDownHandler,IPointerUpHandler
+{
+    [SerializeField] private Image _img;
+    [SerializeField] private Sprite _default, _pressed;
+    [SerializeField] private AudioClip _compressClip, _uncompressClip;
+    [SerializeField] private AudioSource _source;
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        _img.sprite = _pressed;
+        _source.PlayOneShot(_compressClip);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        _img.sprite = _default;
+        _source.PlayOneShot(_uncompressClip);
+    }
+    
+    public void IwasClicked()
+    {
+        Debug.Log("I was clicked");
+    }
+}
